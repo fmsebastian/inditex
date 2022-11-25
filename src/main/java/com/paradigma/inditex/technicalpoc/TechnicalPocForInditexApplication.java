bb0d0931d654508d66d9926f023d265332d16065
Francisco Manuel Sebastian Maldonado <fransebastianmaldonado@gmail.com>
2022-11-25 11:46:34 +0100

    Initial commit with spring initializer

---
+++ src/main/java/com/paradigma/inditex/technicalpoc/TechnicalPocForInditexApplication.java
@@ -0,0 +1,13 @@
+package com.paradigma.inditex.technicalpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalPocForInditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalPocForInditexApplication.class, args);
+	}
+
+}
